5c4ffed6050932daae69026db9bd266e245bafad
AngelVillalobosS <angel.villalobos2269@uppuebla.edu.mx>
2025-05-27 02:43:56 -0600

    LastCommit

--- app/src/main/java/com/example/sumarnumeros/divView.java
+++ app/src/main/java/com/example/sumarnumeros/divView.java
@@ -1,6 +1,12 @@
 package com.example.sumarnumeros;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class divView extends AppCompatActivity {
+    private Button backBtn, nextBtn, divBtn;
+    private EditText num1, num2;
+    private TextView rstView;
+    private MediaPlayer mPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,52 @@ public class divView extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+//        Conexion del mPlayer
+                mPlayer = MediaPlayer.create(this, R.raw.minecraft_click); // Se llama un sonido para cada boton
+//        Conexion de fieldtext con atributos
+        num1 = findViewById(R.id.editTextNumber3);
+        num2 = findViewById(R.id.editTextNumber4);
+//        Conexion de botones con atributos
+        backBtn = findViewById(R.id.button5);
+        nextBtn = findViewById(R.id.button6);
+        divBtn = findViewById(R.id.button4);
+//        Conexion del display tipo texto
+        rstView = findViewById(R.id.textView3);
+
+        divBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.start();
+                String numb1 = num1.getText().toString();
+                String numb2 = num2.getText().toString();
+                if (numb2 != null && numb2 != null){
+                    int n1 = Integer.parseInt(numb1);
+                    int n2 = Integer.parseInt(numb2);
+                    int rst = n1 / n2;
+                    rstView.setText("Resultado: " + rst);
+                }else{
+                    rstView.setText("Debes primero ingresar los numeros");
+                }
+            }
+        });
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                mPlayer.start();
+                Intent backView = new Intent(divView.this, mainView.class);
+                startActivity(backView);
+            }
+        });
+
+        nextBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mPlayer.start();
+                Intent nextView = new Intent(divView.this, sumView.class);
+                startActivity(nextView);
+            }
         });
     }
 }
